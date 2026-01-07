a7d2782a198c99e6c238eafc2e36fe6fb1c06b9a
Ahmed <ahmed.code.rex@gmail.com>
2026-01-08 01:10:34 +0200

    add project EDU-Learning

--- src/components/allProjects.js
+++ src/components/allProjects.js
@@ -1,5 +1,6 @@
 
 import v14 from "/public/certificates/v14.png";
+import v15 from "/public/courses.png";
 
 // UID
 import { v4 as uuidv4 } from "uuid";
@@ -13,5 +14,14 @@ export const allProjects = [
     link: "www.google.com",
     gitHub: "https://github.com/mohammed-ali-photographer/CairoPackages",
     image: v14,
+  },
+  {
+    category: ["react","tailwind"],
+    id: uuidv4(),
+    title: "EDU-Learning",
+    body: "Interactive Learning Platform - Lectures - Assessment Quizzes - Structured Flow - Instant Results",
+    link: "www.google.com",
+    gitHub: "https://github.com/ahmedcoderex/EDU-Learning",
+    image: v15,
   },
 ];
